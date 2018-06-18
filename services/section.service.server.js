8b0f375de5d3a8018785d6a5415224aa992b4856
Tam Vu <vu.t@husky.neu.edu>
2018-06-18 14:26:37 -0400

    user model + sections + enrollment

---
+++ services/section.service.server.js
@@ -0,0 +1,25 @@
+module.exports = function (app) {
+
+  app.post('/api/course/:courseId/section', createSection);
+  app.get('/api/course/:courseId/section', findSectionsForCourse)
+
+  var sectionModel = require('../models/section/section.model.server');
+
+  function findSectionsForCourse(req, res) {
+    var courseId = req.params['courseId'];
+    sectionModel
+      .findSectionsForCourse(courseId)
+      .then(function (sections) {
+        res.json(sections);
+      })
+  }
+
+  function createSection(req, res) {
+    var section = req.body;
+    sectionModel
+      .createSection(section)
+      .then(function (section) {
+        res.json(section);
+      })
+  }
+};
