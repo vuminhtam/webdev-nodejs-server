da4e1a996ff0020f3a4b5f19a357008e05afdbe9
Tam Vu <vu.t@husky.neu.edu>
2018-06-19 02:02:52 -0400

    unenroll

--- models/enrollment/enrollment.model.server.js
+++ models/enrollment/enrollment.model.server.js
@@ -1,22 +1,27 @@
 var mongoose = require('mongoose');
 var enrollmentSchema = require('./enrollment.schema.server');
 var enrollmentModel = mongoose.model(
-  'EnrollmentModel',
-  enrollmentSchema
+    'EnrollmentModel',
+    enrollmentSchema
 );
 
 function enrollStudentInSection(enrollment) {
-  return enrollmentModel.create(enrollment);
+    return enrollmentModel.create(enrollment);
+}
+
+function removeStudentFromSection(removal) {
+    return enrollmentModel.remove(removal);
 }
 
 function findSectionsForStudent(studentId) {
-  return enrollmentModel
-    .find({student: studentId})
-    .populate('section')
-    .exec();
+    return enrollmentModel
+        .find({student: studentId})
+        .populate('section')
+        .exec();
 }
 
 module.exports = {
-  enrollStudentInSection: enrollStudentInSection,
-  findSectionsForStudent: findSectionsForStudent
+    enrollStudentInSection: enrollStudentInSection,
+    removeStudentFromSection: removeStudentFromSection,
+    findSectionsForStudent: findSectionsForStudent
 };
--- services/section.service.server.js
+++ services/section.service.server.js
@@ -1,58 +1,79 @@
 module.exports = function (app) {
 
-  app.post('/api/course/:courseId/section', createSection);
-  app.get('/api/course/:courseId/section', findSectionsForCourse);
-  app.post('/api/section/:sectionId/enrollment', enrollStudentInSection);
-  app.get('/api/student/section', findSectionsForStudent);
+    app.post('/api/course/:courseId/section', createSection);
+    app.get('/api/course/:courseId/section', findSectionsForCourse);
+    app.post('/api/section/:sectionId/enrollment', enrollStudentInSection);
+    app.post('/api/section/:sectionId/unenroll', removeStudentFromSection());
+    app.get('/api/student/section', findSectionsForStudent);
 
-  var sectionModel = require('../models/section/section.model.server');
-  var enrollmentModel = require('../models/enrollment/enrollment.model.server');
+    var sectionModel = require('../models/section/section.model.server');
+    var enrollmentModel = require('../models/enrollment/enrollment.model.server');
 
-  function findSectionsForStudent(req, res) {
-    var currentUser = req.session.currentUser;
-    var studentId = currentUser._id;
-    enrollmentModel
-      .findSectionsForStudent(studentId)
-      .then(function(enrollments) {
-        res.json(enrollments);
-      });
-  }
+    function findSectionsForStudent(req, res) {
+        var currentUser = req.session.currentUser;
+        var studentId = currentUser._id;
+        enrollmentModel
+            .findSectionsForStudent(studentId)
+            .then(function (enrollments) {
+                res.json(enrollments);
+            });
+    }
 
-  function enrollStudentInSection(req, res) {
-    var sectionId = req.params.sectionId;
-    var currentUser = req.session.currentUser;
-    var studentId = currentUser._id;
-    var enrollment = {
-      student: studentId,
-      section: sectionId
-    };
+    function enrollStudentInSection(req, res) {
+        var sectionId = req.params.sectionId;
+        var currentUser = req.session.currentUser;
+        var studentId = currentUser._id;
+        var enrollment = {
+            student: studentId,
+            section: sectionId
+        };
 
-    sectionModel
-      .decrementSectionSeats(sectionId)
-      .then(function () {
-        return enrollmentModel
-          .enrollStudentInSection(enrollment)
-      })
-      .then(function (enrollment) {
-        res.json(enrollment);
-      })
-  }
+        sectionModel
+            .decrementSectionSeats(sectionId)
+            .then(function () {
+                return enrollmentModel
+                    .enrollStudentInSection(enrollment)
+            })
+            .then(function (enrollment) {
+                res.json(enrollment);
+            })
+    }
 
-  function findSectionsForCourse(req, res) {
-    var courseId = req.params['courseId'];
-    sectionModel
-      .findSectionsForCourse(courseId)
-      .then(function (sections) {
-        res.json(sections);
-      })
-  }
+    function removeStudentFromSection(req, res) {
+        var sectionId = req.params.sectionId;
+        var currentUser = req.session.currentUser;
+        var studentId = currentUser._id;
+        var removal = {
+            student: studentId,
+            section: sectionId
+        };
 
-  function createSection(req, res) {
-    var section = req.body;
-    sectionModel
-      .createSection(section)
-      .then(function (section) {
-        res.json(section);
-      })
-  }
+        sectionModel
+            .incrementSectionSeats(sectionId)
+            .then(function () {
+                return enrollmentModel
+                    .removeStudentFromSection(removal)
+            })
+            .then(function (section) {
+                res.json(section);
+            })
+    }
+
+    function findSectionsForCourse(req, res) {
+        var courseId = req.params['courseId'];
+        sectionModel
+            .findSectionsForCourse(courseId)
+            .then(function (sections) {
+                res.json(sections);
+            })
+    }
+
+    function createSection(req, res) {
+        var section = req.body;
+        sectionModel
+            .createSection(section)
+            .then(function (section) {
+                res.json(section);
+            })
+    }
 };
